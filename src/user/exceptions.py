bb7055a82336f949549d0bf24802c0567288305a
Aleksey Kondratenko <kond@omegafuture.ru>
2023-11-24 22:52:31 +0300

    Hashing password and add ligin method

--- src/user/exceptions.py
+++ src/user/exceptions.py
@@ -3,12 +3,17 @@ from typing import Any
 from fastapi import HTTPException, status
 
 
-class LoginAlreadyExist(HTTPException):
-    def __init__(
-        self,
-        detail: str = "Login already exists!",
-        headers: dict[str, Any] | None = None,
-    ) -> None:
-        super().__init__(
-            status.HTTP_409_CONFLICT, detail=detail, headers=headers
-        )
+LoginAlreadyExistException = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Login already exists.",
+)
+
+BadUserLoginExceptions = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Bad user login."
+)
+
+BadUserPasswordExceptions = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Bad user password."
+)
--- src/user/service.py
+++ src/user/service.py
@@ -1,32 +1,63 @@
+import hashlib
 from typing import Optional
 
+from config import SALT
 from src.base.uow import SqlAlchemyUnitOfWork
 from src.base.exceptions import NotFoundException
-from src.user.exceptions import LoginAlreadyExist
+from src.user.exceptions import LoginAlreadyExistException, BadUserCredentialsException, BadUserLoginExceptions
 from src.user import schemas
 
 
 class UserService:
-    @staticmethod
-    async def create_user(user_dto: schemas.RequestCreateUpdateUser) -> schemas.User:
+
+    async def create_user(self, user_dto: schemas.RequestCreateUpdateUser) -> schemas.User:
+        """Method get user's data and create new user in database"""
+
+        new_user = schemas.ResponseCreateUpdateUser(
+            name=user_dto.name,
+            surname=user_dto.surname,
+            login=user_dto.login,
+            password_hash=self.__hashing_password(password=user_dto.password_hash)
+        )
         async with SqlAlchemyUnitOfWork() as uow:
             if await uow.user.get_by_login_or_none(login=user_dto.login):
-                raise LoginAlreadyExist()
-            new_user = await uow.user.create(data=user_dto.model_dump())
+                raise LoginAlreadyExistException()
+            created_user = await uow.user.create(data=new_user)
             await uow.commit()
-        return new_user
+        return created_user
 
     @staticmethod
     async def get_or_404(id: int) -> Optional[schemas.User]:
+        """Method get user's id and return user or raise 404 error"""
+
         async with SqlAlchemyUnitOfWork() as uow:
             user = await uow.user.read(id=id)
             uow.commit()
         if not user:
-            raise NotFoundException(detail=f'User not found by id {id}')
+            raise NotFoundException(detail=f'User with id {id} not found.')
         return user
 
     @staticmethod
     async def get_list() -> list[schemas.ResponseCreateUpdateUser]:
+        """Method return list of all users"""
+
         async with SqlAlchemyUnitOfWork() as uow:
             return await uow.user.list()
 
+    async def login_or_409(self, user_credentials: schemas.UserCredentials) -> schemas.ResponseCreateUpdateUser:
+        """Method login user or raise 409 error"""
+
+        async with SqlAlchemyUnitOfWork() as uow:
+            user = await uow.user.get_by_login_or_none(login=user_credentials.login)
+        if not user:
+            raise BadUserLoginExceptions
+        if self.__hashing_password(password=user_credentials.password) != user.password_hash:
+            raise BadUserCredentialsException
+        return user
+
+    @staticmethod
+    async def __hashing_password(password: str) -> str:
+        """Method get user's password and return hashed password with salt"""
+
+        convert_password = str.encode(password + str(SALT), encoding='utf-8')
+        return hashlib.md5(convert_password).hexdigest()
